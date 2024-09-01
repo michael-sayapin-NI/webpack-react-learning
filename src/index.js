44d46050f4ddef1db0181de4c17204acf93be7bb
Michael Sayapin <michael.sayapin@naturalint.com>
2024-09-01 23:47:17 +0300

    feat: add react component and bundle it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello, world!');
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <h1>Rick and Morty API</h1>
+}
+
+const rootElement = document.getElementById('root');
+const container = ReactDOM.createRoot(rootElement);
+container.render(<App />);
